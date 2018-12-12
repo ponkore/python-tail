e35d1bf87006fee52afe46bf4a6baaba67387ea7
ponkore <ponkore@gmail.com>
2018-12-12 11:17:08 +0900

    remove trailing whitespace, add blank lines

--- tail.py
+++ tail.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-Python-Tail - Unix tail follow implementation in Python. 
+Python-Tail - Unix tail follow implementation in Python.
 
 python-tail can be used to monitor changes to a file.
 
@@ -11,11 +11,11 @@ Example:
     # Create a tail instance
     t = tail.Tail('file-to-be-followed')
 
-    # Register a callback function to be called when a new line is found in the followed file. 
+    # Register a callback function to be called when a new line is found in the followed file.
     # If no callback function is registerd, new lines would be printed to standard out.
     t.register_callback(callback_function)
 
-    # Follow the file with 5 seconds as sleep time between iterations. 
+    # Follow the file with 5 seconds as sleep time between iterations.
     # If sleep time is not provided 1 second is used as the default time.
     t.follow(s=5) '''
 
@@ -26,12 +26,13 @@ import os
 import sys
 import time
 
+
 class Tail(object):
     ''' Represents a tail command. '''
     def __init__(self, tailed_file, encoding='utf-8'):
         ''' Initiate a Tail instance.
             Check for file validity, assigns callback function to standard out.
-            
+
             Arguments:
                 tailed_file - File to be followed.
                 encoding - input file encoding'''
@@ -42,15 +43,15 @@ class Tail(object):
         self.encoding = encoding
 
     def follow(self, s=1):
-        ''' Do a tail follow. If a callback function is registered it is called with every new line. 
+        ''' Do a tail follow. If a callback function is registered it is called with every new line.
         Else printed to standard out.
-    
+
         Arguments:
             s - Number of seconds to wait between each iteration; Defaults to 1. '''
 
         with open(self.tailed_file, encoding=self.encoding) as file_:
             # Go to the end of file
-            file_.seek(0,2)
+            file_.seek(0, 2)
             while True:
                 curr_position = file_.tell()
                 line = file_.readline()
@@ -73,8 +74,10 @@ class Tail(object):
         if os.path.isdir(file_):
             raise TailError("File '%s' is a directory" % (file_))
 
+
 class TailError(Exception):
     def __init__(self, msg):
         self.message = msg
+
     def __str__(self):
         return self.message
